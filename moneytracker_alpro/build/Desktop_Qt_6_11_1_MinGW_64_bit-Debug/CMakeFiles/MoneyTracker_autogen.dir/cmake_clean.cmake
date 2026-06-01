df7056960b453f16ccb3f90327229aab84cf6bb8
YuvensiusMotifasiDaeli-251401025 <yuvensius1206@gmail.com>
2026-06-01 22:43:31 +0700

    Hasil Akhir

---
+++ moneytracker_alpro/build/Desktop_Qt_6_11_1_MinGW_64_bit-Debug/CMakeFiles/MoneyTracker_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MoneyTracker_autogen"
+  "MoneyTracker_autogen/mocs_compilation.cpp"
+  "MoneyTracker_autogen/timestamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/MoneyTracker_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ moneytracker_alpro/build/Desktop_Qt_6_11_1_MinGW_64_bit-Debug/CMakeFiles/MoneyTracker_autogen.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty custom commands generated dependencies file for MoneyTracker_autogen.
+# This may be replaced when dependencies are built.
